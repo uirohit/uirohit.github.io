63038960abd8c8ffa250f2e7ad099083cbfedeb9
uirohit <uiuxrohit@gmail.com>
2024-11-19 12:46:41 +0530

    rohit

---
+++ footer.php
@@ -0,0 +1,111 @@
+    <!-- FOOTER AREA -->
+
+<footer class="rts-footer site-footer-one section__padding">
+  
+        <div class="container">
+            <div class="row">
+                <div class="rts-footer__copyright text-center">
+                    <p>© ROHIT SINGH</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+    <!-- FOOTER AREA END -->
+
+    <div id="anywhere-home" class="">
+    </div>
+
+    <!-- side bar area  -->
+    <div id="side-bar" class="side-bar header-two">
+        <button class="close-icon-menu"><i class="fa-sharp fa-thin fa-xmark"></i></button>
+        <!-- mobile menu area start -->
+        <div class="mobile-menu-main">
+            <nav class="nav-main mainmenu-nav mt--30">
+                <ul class="mainmenu metismenu" id="mobile-menu-active">
+                    <li class="menu-item"><a href="https://www.linkedin.com/in/rohitsinghuiux/" target="_blank" class="FDC-dropdown-main-element">Linkedin</a></li>
+                                <li class="menu-item"><a href="https://www.youtube.com/@RohitUXD/videos" target="_blank" class="FDC-dropdown-main-element">Youtube</a></li>
+                </ul>
+            </nav>
+
+           
+        </div>
+        <!-- mobile menu area end -->
+    </div>
+    <!-- side abr area end -->
+
+
+   
+    <!-- BACK TO TOP AREA START -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;"></path>
+        </svg>
+    </div>
+    <!-- BACK TO TOP AREA EDN -->
+
+    <!-- jquery js -->
+    <script defer src="assets/js/plugins/jquery.min.js"></script>
+    <!-- <script defer src="assets/js/plugins/popper.js"></script> -->
+    <!-- <script defer src="assets/js/plugins/bootstrap.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script> -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+
+    <script defer src="assets/js/plugins/metismenu.js"></script>
+    <!-- <script defer src="assets/js/vendor/jqueryui.js"></script> -->
+    <script defer src="assets/js/vendor/waypoint.js"></script>
+    <script defer src="assets/js/vendor/locomotive.js"></script>
+    <!-- <script defer src="assets/js/plugins/swiper.js"></script> -->
+    <!-- <script defer src="assets/js/plugins/theia-sticky-sidebar.min.js"></script> -->
+    <script defer src="assets/js/plugins/jquery.nice-select.min.js"></script>
+    <script defer src="assets/js/plugins/sal.js"></script>
+    <script defer src="assets/js/vendor/waw.js"></script>
+    <script defer src="assets/js/plugins/counter-up.js"></script>
+    <!-- <script defer src="assets/js/plugins/magnific-popup.js"></script> -->
+    <script defer src="https://unpkg.com/gsap@3/dist/gsap.min.js"></script>
+    <script defer src="https://unpkg.com/gsap@3/dist/ScrollTrigger.min.js"></script>
+    <!-- contact form js -->
+    <!-- <script defer src="assets/js/plugins/contact-form.js"></script> -->
+    <script defer src="assets/js/main.js"></script>
+
+
+   <!--  <script type="text/javascript">
+        $(window).load(function() {
+  $("body").removeClass("preload");
+});
+    </script>  -->
+
+
+<!-- <script>
+    window.addEventListener('load', function() {
+  const animatedElement = document.querySelector('.animated');
+  if (animatedElement) {
+    animatedElement.classList.add('start-animation');
+  }
+});
+
+</script>
+ -->
+
+<!-- 
+// script.js
+$(window).on('load', function() {
+  $('body').removeClass('preload1');
+  $('.element').addClass('visible');
+});
+</script>
+ -->
+
+<sc
+
+</body>
+
+
+
+</html>
+
