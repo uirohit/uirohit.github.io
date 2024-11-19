eef3ebd0c40d47dfe0f495a12ac96e6eb7066277
uirohit <uiuxrohit@gmail.com>
2024-11-19 13:39:38 +0530

    site

---
+++ fdc/bckp/edge-computing.php
@@ -0,0 +1,359 @@
+<?php include_once 'header.php'; ?>
+
+
+
+<div class="rts-hosting-banner rts-hosting-banner-bg image-edge">
+        <div class="container">
+            <div class="row">
+                <div class="banner-area">
+                    <div class="rts-hosting-banner rts-hosting-banner__content about__banner">
+                    
+                       <!--  <h1 class="banner-title sal-animate" data-sal="slide-down" data-sal-delay="200" data-sal-duration="800">
+                          Edge Computing Solutions
+                        </h1> -->
+                          <h1 class="banner__title " data-sal="slide-right" data-sal-delay="1000" data-sal-duration="1000" style="color: #fff;font-size: 44px;">
+                              Edge Computing Services
+                        </h1>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- <section class="rts-hero-four rts-hero__four" style="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-xl-6 col-md-10">
+                    <div class="rts-hero-four__content " data-sal="slide-right" data-sal-delay="1000" data-sal-duration="1000" data-sel-easing="ease-out-bounce">
+                
+                 
+                        <h1 class="banner__title " data-sal="slide-right" data-sal-delay="1000" data-sal-duration="1000">
+                      Our Story
+                        </h1>
+                       
+                        
+                 
+                    </div>
+                </div>
+            </div>
+              
+        </div>
+    </section> -->
+
+   
+    
+<!-- 
+<section class="rts-hero-four " style="background-image: url(assets/images/edgebanner.jpg);padding: 180px 0px;">
+        <div class="container ">
+<div class="row ">
+                <div class="col-lg-7">
+                    <div class="rts-faq__first rts-faq__first2">
+                      
+                        <h1 class="banner__title " data-sal="slide-right" data-sal-delay="1000" data-sal-duration="1000" style="color: #fff;font-size: 44px;">
+                     Edge Computing Solutions
+                        </h1>
+                   
+
+        
+
+                        </div>
+                    </div>
+
+                </div>
+           
+          
+        </div>
+    </section>
+ -->
+
+<section class="rts-contact-form pt-120 image-bg" style="background: linear-gradient(109.6deg, rgb(255 212 4) 11.2%, rgb(230 239 0) 100.2%);position: relative;">
+        <div class="container">
+            <div class="col-12 d-flex justify-content-center">
+                    <div class="rts-section text-center">
+                    <!-- <div class="rts-section text-center"> -->
+                       <h2 class="title" data-sal="slide-down" data-sal-delay="800" data-sal-duration="800" style="color:#000;">
+                 We offer cutting-edge Edge Computing Solutions
+                        </h2>
+                       <p data-sal="slide-down" data-sal-delay="800" data-sal-duration="800" class="w-790" style="color:#000;margin: auto;" >At Fortune Data Center, we offer cutting-edge Edge Computing Solutions designed to bring processing power closer to where data is generated, enabling faster and more efficient data handling.</p><br />
+
+                 
+                    </div>
+                </div>
+          
+        </div>
+    </section>
+
+
+
+<!-- <section class="rts-hero-four rts-hero__four mern__hosting" style="background-image: url(assets/images/bg4.jpg);">
+        <div class="container container2">
+<div class="row ">
+                <div class="col-lg-7">
+                    <div class="rts-faq__first rts-faq__first2">
+                        <h2 class="title sal-animate" data-sal="slide-down" data-sal-delay="300" data-sal-duration="800" style="color:#fff;text-align: left;">
+                           Decade of relentless dedication
+                        </h2>
+                       <p data-sal="slide-down" data-sal-delay="200" data-sal-duration="800" class="sal-animate" style="color:#fff;text-align: left;">Fortune Group has carved a niche for itself in the industry, driven by unwavering passion and a commitment to setting new standards of excellence in product quality.</p><br />
+
+                       <p data-sal="slide-down" data-sal-delay="200" data-sal-duration="800" class="sal-animate" style="color:#fff;text-align: left;">Building new possibilities by providing secure and scalable colocation services that empower businesses to grow seamlessly. Our edge computing solutions bring processing power closer to the data source, enhancing performance for real-time applications.
+ </p><br /><br />
+
+        
+
+                        </div>
+                    </div>
+
+                </div>
+           
+          
+        </div>
+    </section>
+ -->
+
+
+
+
+<section class="dotted_bg">
+<section class="rts-faq section__padding rts-brand rts-brand__bg--section">
+        <div class="container">
+            <div class="row ">
+
+                  <div class="col-12 d-flex justify-content-center">
+                    <div class="text-center">
+                       <h2 class="title " data-sal="slide-down" data-sal-delay="800" data-sal-duration="800" style="color:#000;">
+                           Services We Offered
+                        </h2>
+                       <p data-sal="slide-down" data-sal-delay="800" data-sal-duration="800" class="w-790" style="color:#000;margin: auto;" >Our services are tailored to meet the needs of various industries, providing enhanced performance, reduced latency, and greater reliability. Here’s a closer look at the Edge Computing Solutions we provide:
+</p><br />
+
+        
+                    </div>
+
+                </div>
+
+
+
+
+<div class="row justify-content-sm-center justify-content-md-start g-30 container2">
+                <div class="col-lg-6 col-md-6 col-sm-10" data-sal="slide-left" data-sal-delay="300" data-sal-duration="800">
+                    <div class="rts-blog__single">
+                        <a href="blog-details.html">
+                            <img class="blog__thumb" src="assets/images/edge-one.jpg" alt="blog post thumb">
+                        </a>
+                        <div class="rts-blog__single--meta">
+                         
+                            <a href="#" class="title">Content Delivery Network</a>
+
+                            <div class="rts-blog__single--author">
+                                <div class="author">
+                                     <p>Enhance your digital content delivery with our robust <b>CDN services</b>. We provide a network of distributed servers that deliver content to users based on their geographic location. </p>
+
+                                     <p>Ensuring faster load times and improved user experience. Our CDN solutions are ideal for <b>websites, streaming services, and online platforms</b> seeking to optimise content delivery.</p>
+                       
+                                </div>
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             
+        
+                <div class="col-lg-6 col-md-6 col-sm-10" data-sal="slide-right" data-sal-delay="300" data-sal-duration="800">
+                    <div class="rts-blog__single">
+                        <a href="blog-details.html">
+                            <img class="blog__thumb" src="assets/images/edge-two.jpg" alt="blog post thumb">
+                        </a>
+                        <div class="rts-blog__single--meta">
+                         
+                            <a href="#" class="title">Over-the-Top (OTT) Services
+</a>
+
+                            <div class="rts-blog__single--author">
+                                <div class="author">
+                                     <p>Transform your media delivery with our OTT services. We offer comprehensive solutions for streaming video, audio, and other media content directly to consumers over the internet.</p>
+
+                                     <p>  Our OTT services provide high-quality, scalable, and secure streaming capabilities, enabling you to reach a broader audience with your digital content.</p><br />
+                             
+                                </div>
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+               
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+                
+            </div><br /><br />
+
+
+
+
+
+        </section>
+    </section>
+
+
+
+<section class="rts-contact-form pt-120 image-bg" style="background: linear-gradient(109.6deg, rgb(255, 230, 109) 11.2%, rgb(87, 232, 107) 100.2%);;position: relative;">
+
+     <div class="container2" data-sal="slide-top" data-sal-delay="300" data-sal-duration="800">
+            <div class="col-lg-12">
+                    <div class="rts-faq__first rts-faq__first2">
+                        <h2 class="title " data-sal="slide-down" data-sal-delay="300" data-sal-duration="800">
+                  Gaming & AR/VR
+                        </h2>
+                       <p data-sal="slide-down" data-sal-delay="1000" data-sal-duration="800" class="sal-animate">Deliver immersive gaming and augmented/virtual reality experiences with our edge computing solutions.
+                        </div>
+                    </div><br />
+            <div class="row gy-40 justify-content-md-center">
+
+               
+                 <div class="col-lg-8 col-md-8 text-center">
+                    <div class="">
+                        <div class="">
+                      <div>     <img src="assets/images/ar.png" alt="" style=""></div>
+                                      
+                          <p class="hosting-feature__desc text-center"></p><br />
+                               <p class="hosting-feature__desc black" data-sal="slide-down" data-sal-delay="1000" data-sal-duration="800">We provide the low-latency and high-performance infrastructure required to support real-time gaming and AR/VR applications. Our solutions enhance user experiences by reducing lag and ensuring smooth, responsive gameplay and interactions.</p>
+                        </div>
+             
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-12 ">
+                    <div class="">
+                        <!-- <h3 class="hosting-feature__title font-36 sal-animate" data-sal="slide-down" data-sal-delay="100" data-sal-duration="800">Leadership</h3> -->
+
+                     
+
+
+                   
+
+                        </div>
+                </div>
+
+               
+            </div>
+
+
+
+        </div>
+    
+
+    </section>
+
+
+
+<section class="dotted_bg">
+<section class="rts-faq section__padding rts-brand rts-brand__bg--section">
+        <div class="container">
+            <div class="row ">
+
+
+
+
+<div class="row justify-content-sm-center justify-content-md-start g-30 ">
+                <div class="col-lg-4 col-md-4 col-sm-10" data-sal="slide-left" data-sal-delay="300" data-sal-duration="800">
+                    <div class="rts-blog__single">
+                        <a href="blog-details.html">
+                            <img class="blog__thumb" src="assets/images/ai.jpg" alt="blog post thumb">
+                        </a>
+                        <div class="rts-blog__single--meta">
+                         
+                            <a href="#" class="title">AI Services</a>
+
+                            <div class="rts-blog__single--author">
+                                <div class="author">
+                                     <p>Leverage the power of artificial intelligence with our AI services. We provide advanced AI solutions that enable you to process and analyse data at the edge, delivering real-time insights and automation.</p>
+
+                                     <p> Our AI services support various applications, including predictive maintenance, customer behaviour analysis, and intelligent automation, enhancing your business operations.</p>
+                       
+                                </div>
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             
+        
+                <div class="col-lg-4 col-md-4 col-sm-10" data-sal="slide-left" data-sal-delay="300" data-sal-duration="800">
+                    <div class="rts-blog__single">
+                        <a href="#">
+                            <img class="blog__thumb" src="assets/images/iot.jpg" alt="blog post thumb">
+                        </a>
+                        <div class="rts-blog__single--meta">
+                         
+                            <a href="#" class="title">IoT & Edge Computing
+</a>
+
+                            <div class="rts-blog__single--author">
+                                <div class="author">
+                                     <p>Optimise your Internet of Things (IoT) deployments with our managed IoT and edge computing services. We offer end-to-end solutions for managing IoT devices and processing data at the edge, reducing latency and improving efficiency.</p>
+
+                                     <p>Our services ensure seamless connectivity, robust security, and efficient management of your IoT infrastructure.</p><br />
+                             
+                                </div>
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+               
+
+                      <div class="col-lg-4 col-md-4 col-sm-10" data-sal="slide-left" data-sal-delay="300" data-sal-duration="800">
+                    <div class="rts-blog__single">
+                        <a href="#">
+                            <img class="blog__thumb" src="assets/images/analytics.jpg" alt="blog post thumb">
+                        </a>
+                        <div class="rts-blog__single--meta">
+                         
+                            <a href="#" class="title">Analytics
+</a>
+
+                            <div class="rts-blog__single--author">
+                                <div class="author">
+                                     <p>Gain valuable insights from your data with our edge analytics services. We provide advanced analytics capabilities that enable you to process and analyse data at the edge, delivering real-time insights and actionable information. </p>
+
+                                     <p>Our analytics solutions support various use cases, including predictive analytics, real-time monitoring, and decision-making support.</p>
+                             
+                                </div>
+                        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+</div>
+</div>
+</section>
+</section>
+
+<br /><br />
+  <?php include_once 'form.php'; ?>
+
+<?php include_once 'footer.php'; ?>
+
+
+
---
+++ fdc/industries.php
@@ -0,0 +1,259 @@
+<div class="rts-why-choose bg-white section__padding" style="    background: radial-gradient(circle, #e1e1e1 1px, transparent 1px), radial-gradient(circle, #e1e1e1 1px, transparent 1px);
+    background-size: 20px 20px;
+    background-position: 0px 0, 0px 4px;">
+        <div class="container">
+              <div class="row ">
+                <div class="col-lg-12">
+                    <div class="rts-faq__first rts-faq__first2">
+                        <h2 class="title" data-sal="slide-down" data-sal-delay="300" data-sal-duration="800">
+                           Industries We Serve
+                        </h2>
+                       <p data-sal="slide-down" data-sal-delay="200" data-sal-duration="800" class="">What sets Fortune Data Centre apart
+</p>
+
+                        </div>
+                    </div>
+                </div><br /><br />
+           <!--  <div class="row">
+                <div class="rts-section section-style-two">
+                    <div class="rts-section__two">
+                        <h2 class="title mb-0 sal-animate" data-sal="slide-down" data-sal-delay="100" data-sal-duration="800">What sets 
+Fortune Data Centre apart</h2>
+                    </div>
+                    <div class="rts-section-description">
+                        <p class="description w-400 sal-animate" data-sal="slide-left" data-sal-delay="200" data-sal-duration="800">
+                     We guarantee high availability and uptime to keep your business running smoothly.
+                        </p>
+                    </div>
+                </div>
+            </div> -->
+            <!-- why choose feature -->
+
+
+                <div class="row">
+                <div class="rts-service__wrapper">
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon shared__hosting indus" style="">
+                                <img src="assets/images/usp/ai.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Enterprises</a>
+                                </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon email__hosting indus">
+                                <img src="assets/images/usp/edge.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Government
+</a>
+                           
+                      
+                        </div>
+                        <!-- single service end -->
+                    </div>
+
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon wordpress__hosting indus">
+                                <img src="assets/images/usp/custom-sol.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">BFSI
+</a>
+                           
+                           
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon dedicated__hosting indus">
+                                <img src="assets/images/usp/support.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Metal & Mining
+
+</a>
+                          
+                           
+                        </div>
+                        <!-- single service end -->
+
+                    </div>
+
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon vps__hosting indus">
+                                <img src="assets/images/usp/price-transparency.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">SMB
+</a>
+                           
+                      
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon eccomerce__hosting indus">
+                                <img src="assets/images/usp/credit-facility.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Manufacturing
+</a>
+                            
+                           
+                        </div>
+                        <!-- single service end -->
+                    </div>
+
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon reseller__hosting indus">
+                                <img src="assets/images/usp/recognition.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Automobiles
+</a>
+                           
+                    
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon cloud__hosting indus">
+                                <img src="assets/images/usp/minimal-charge.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Chemical
+</a>
+                         
+                       
+                        </div>
+                        <!-- single service end -->
+                    </div>
+
+
+
+
+
+                </div>
+
+                    <div class="rts-service__wrapper">
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon shared__hosting indus">
+                                <img src="assets/images/usp/ai.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Pharma</a>
+                         
+                  
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon email__hosting indus">
+                                <img src="assets/images/usp/edge.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">FMCG
+</a>
+                           
+                      
+                        </div>
+                        <!-- single service end -->
+                    </div>
+
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon wordpress__hosting indus">
+                                <img src="assets/images/usp/custom-sol.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Health Cares
+</a>
+                           
+                           
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon dedicated__hosting indus">
+                                <img src="assets/images/usp/support.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Startups
+
+</a>
+                          
+                           
+                        </div>
+                        <!-- single service end -->
+
+                    </div>
+
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon vps__hosting indus">
+                                <img src="assets/images/usp/price-transparency.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Retail & Ecommerce
+</a>
+                           
+                      
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon eccomerce__hosting indus">
+                                <img src="assets/images/usp/credit-facility.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Online Gaming 
+</a>
+                            
+                           
+                        </div>
+                        <!-- single service end -->
+                    </div>
+
+                    <div class="rts-service__column">
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon reseller__hosting indus">
+                                <img src="assets/images/usp/recognition.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Online Education
+</a>
+                           
+                    
+                        </div>
+                        <!-- single service end -->
+                        <!-- single service -->
+                        <div class="rts-service__single rts-service__single2">
+                            <div class="rts-service__single--icon cloud__hosting indus">
+                                <img src="assets/images/usp/minimal-charge.svg" alt="">
+                            </div>
+                            <a href="#" class="rts-service__single--title rts-service__single--title3">Media
+</a>
+                         
+                       
+                        </div>
+                        <!-- single service end -->
+                    </div>
+
+
+
+
+
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+    </div>
